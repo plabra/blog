31e0dcfa47f9c680ba736c0156842519941b8c94
plabra <89382192+plabra@users.noreply.github.com>
2022-02-11 22:22:45 +0800

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,10 +1,4 @@
-// sign in //
-const para = document.querySelector('p');
-para.addEventListener('click', updateName);
-function updateName() {
-  let name = prompt('请输入匿称');
-  para.textContent = '匿称:' + name;
-}
+
 
 // upload blogs //
 // Create needed constants
