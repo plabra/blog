5d8114476d438c41c484ca67a32302350b5f2c86
plabra <89382192+plabra@users.noreply.github.com>
2022-02-11 22:23:32 +0800

    Delete pre.js

--- pre.js
+++
@@ -1,5 +0,0 @@
-const list = document.querySelector('ul');
-const titleInput = document.querySelector('#title');
-const bodyInput = document.querySelector('#body');
-const form = document.querySelector('form');
-const submitBtn = document.querySelector('form button');
