1a685b207aa4c18f787b8227d65e48c43e99e11e
plabra <89382192+plabra@users.noreply.github.com>
2022-02-20 18:03:56 +0800

    Delete blog2.js

--- blog2.js
+++
@@ -1,9 +0,0 @@
-var sect = document.querySelector('section');
-var li = document.createElement('li');
-li.textContent = 'We hope you enjoyed the ride.';
-sect.appendChild(li);
-var text = document.createTextNode(' — the premier source for web development knowledge.');
-var linkLi = document.querySelector('p');
-linkLi.appendChild(text);
-sect.removeChild(linkLi);
-linkLi.parentNode.removeChild(linkLi);
