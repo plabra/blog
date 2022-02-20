3efe84badbaee35b1a2e11c91c8a309ed22ce338
plabra <89382192+plabra@users.noreply.github.com>
2022-02-20 17:42:21 +0800

    Create blog2.js

---
+++ blog2.js
@@ -0,0 +1,7 @@
+var sect = document.querySelector('section');
+var para = document.createElement('p');
+para.textContent = 'We hope you enjoyed the ride.';
+sect.appendChild(para);
+var text = document.createTextNode(' — the premier source for web development knowledge.');
+var linkPara = document.querySelector('p');
+linkPara.appendChild(text);
