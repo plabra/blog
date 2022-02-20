b5b922c258ea939206645876439a93278ad83c67
plabra <89382192+plabra@users.noreply.github.com>
2022-02-20 17:51:27 +0800

    Update blog2.js

--- blog2.js
+++ blog2.js
@@ -1,7 +1,8 @@
 var sect = document.querySelector('section');
-var li = document.createElement('li');
-li.textContent = 'We hope you enjoyed the ride.';
-sect.appendChild(li);
-var text = document.createTextNode(' — the premier source for web development knowledge.');
-var linkLi = document.querySelector('p');
-linkLi.appendChild(text);
+//var li = document.createElement('li');
+//li.textContent = 'We hope you enjoyed the ride.';
+//sect.appendChild(li);
+//var text = document.createTextNode(' — the premier source for web development knowledge.');
+//var linkLi = document.querySelector('p');
+//linkLi.appendChild(text);
+sect.removeChild(linkLi);
