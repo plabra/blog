18e509badcc67d51a956ff9458e7596fba0b2098
plabra <89382192+plabra@users.noreply.github.com>
2022-03-20 20:42:54 +0800

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -192,3 +192,12 @@ window.onload = function() {
   }
 
 };
+
+//get time
+function mytime(){
+var a = new Date();
+var b = a.toLocaleTimeString();
+var c = a.toLocaleDateString();
+document.getElementById("time").innerHTML = c+"&nbsp"+b;
+}
+setInterval(function() {mytime()},1000);
