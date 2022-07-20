82fc1fe0041025f8bf3de15447e606ff95286409
plabra <89382192+plabra@users.noreply.github.com>
2022-07-20 10:56:04 +0800

    Update backtotop.js

--- backtotop.js
+++ backtotop.js
@@ -4,16 +4,16 @@ let totop = document.querySelector('.totop')
       // 获取浏览器卷去的高度
       let high = document.documentElement.scrollTop || document.body.scrollTop
       if (high >= 50) {
-        totop.style.display = 'block'
+        
         $.('.totop').fadeIn(50);
       } else {
-        totop.style.display = 'none'
+      
         $.('.totop').fadeOut(50);
       }
     }
     // 点击返回顶部，可以加动画使其慢慢回到顶部
     totop.addEventListener('click',() => {
-      document.documentElement.scrollTop = 0
-      document.body.scrollTop = 0
+  
+      
       $('html,body').animate.({scrollTop: "0px" });
     })
