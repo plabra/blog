7866d278b3fe26c929515d094f7ebe16afd2c268
plabra <89382192+plabra@users.noreply.github.com>
2022-08-10 12:18:17 +0800

    Update unhidden.js

--- unhidden.js
+++ unhidden.js
@@ -1,20 +1,20 @@
 function spanClick(){
      var obj=document.getElementById("details1");
-     var att=obj.getAttribute("open");
-     if(att!="open"){
-         obj.setAttribute("open","open");
+     var att=obj.getAttribute("open1");
+     if(att!="open1"){
+         obj.setAttribute("open1","open1");
          }
      else{
-         obj.removeAttribute("open");
+         obj.removeAttribute("open1");
          }
      }
 function spanClick(){
      var obj=document.getElementById("details2");
-     var att=obj.getAttribute("open");
-     if(att!="open"){
-         obj.setAttribute("open","open");
+     var att=obj.getAttribute("open2");
+     if(att!="open2"){
+         obj.setAttribute("open2","open2");
          }
      else{
-         obj.removeAttribute("open");
+         obj.removeAttribute("open2");
          }
      }
