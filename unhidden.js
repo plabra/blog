5ba10be46512e08b313d6c9229e507249510176a
plabra <89382192+plabra@users.noreply.github.com>
2022-08-10 11:52:40 +0800

    Update unhidden.js

--- unhidden.js
+++ unhidden.js
@@ -8,3 +8,13 @@ function spanClick(){
          obj.removeAttribute("open");
          }
      }
+function spanClick(){
+     var obj=document.getElementById("details2");
+     var att=obj.getAttribute("open");
+     if(att!="open"){
+         obj.setAttribute("open","open");
+         }
+     else{
+         obj.removeAttribute("open");
+         }
+     }
