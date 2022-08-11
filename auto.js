3f9470f05050cfc8d04999d18598f7469cc0a037
plabra <89382192+plabra@users.noreply.github.com>
2022-08-11 20:12:22 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -20,4 +20,7 @@ window.util = util;
 if( !util.isMB() ){
 window.location.href = 'https://plabra.github.io/main/index2.html';
 
+}else{
+window.location.href = 'https://plabra.github.io/main';
+}
 })();
