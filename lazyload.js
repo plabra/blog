3d868d3145a05d574cf75b23f1615b9d52481fa4
plabra <89382192+plabra@users.noreply.github.com>
2023-09-30 10:27:42 +0800

    Update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -1,11 +1,16 @@
-let imgs = [...document.querySelectorAll('img')];
- window.addEventListener('scroll', function () {
-    imgs.forEach(img => {
-      if (img.getBoundingClientRect().top < window.innerHeight) { 
-     
-        let dataSrc = img.getAttribute(' data-src'); // 获取 data-src 真实的地址
-        img.setAttribute('src', dataSrc); // 将 真实的地址 替换为 src属性
-        console.log(img.src);
-      }
+window.addEventListener('scroll', function () {
+    let imgs = [...document.querySelectorAll('.img')]
+    const callback = entries => { // entries 是观察的元素数组
+        entries.forEach(ele => {
+            if (ele.isIntersecting) { // isIntersecting  是否被观察到
+                const data_src = ele.target.getAttribute('data-src'); //这里基本和 Method1/Method2一样
+                ele.target.setAttribute('src', data_src); // ele.target 是目标元素
+                observe.unobserve(ele.target) // 真实地址替换后 取消对它的观察
+            }
+        })
+    };
+    const observe = new IntersectionObserver(callback); // 实例化 IntersectionObserver
+    imgs.forEach(image => { 
+        observe.observe(image) // observe : 被调用的IntersectionObserver实例。给每个图片添加观察实例
     })
-  })
+  }
